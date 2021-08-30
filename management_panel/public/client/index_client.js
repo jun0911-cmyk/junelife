e113bfa15e26fec63667a4959677ecde3069f052
jun0911 <jun.0911@daum.net>
2021-08-31 01:12:42 +0900

    main page css development

--- management_panel/public/client/index_client.js
+++ management_panel/public/client/index_client.js
@@ -1,9 +1,7 @@
-$('#isLogin').hide();
-
 $(function() {
     $.ajax({
         type: 'GET',
-        url: '/:id',
+        url: '/',
         beforeSend: function (xhr) {
             xhr.setRequestHeader("Content-type","application/json");
             xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('accessToken'));
@@ -15,16 +13,41 @@ $(function() {
             var status = result.status;
             var accessToken = result.newAccessToken;
             if (status == false || err) {
-                location.href = '/user/login';
-            } else if (status == '-1') {
-                alert('로그아웃 되셨습니다.');
-                location.href = '/user/logout';
+                location.href = '/access_warnings';
             } else if (status == true) {
-                $('#isLogin').show();
-                $('#isNotLogin').hide();
                 if (accessToken) {
                     localStorage.setItem('accessToken', accessToken);
                 }
+                Vue.component('account-component', {
+                    template: `
+                        <div class="dropdown">
+                            <a href="javascript:void(0)" class="singup">
+                                <i class="fas fa-user-circle" style="font-size: 27px; margin-top: 10px"></i>
+                            </a>
+                            <div class="dropdown-content">
+                                <a href="/student/check/class">처리 이력</a>
+                                <a href="#">내 정보</a>
+                                <a href="#">정보 변경</a>
+                                <a href="/user/logout">로그아웃<i class="fas fa-sign-out-alt"></i></a>
+                            </div>
+                        </div>
+                    `
+                });
+
+                Vue.component('bell-component', {
+                    template: `
+                        <i class="fas fa-bell" style="font-size: 27px; margin-left: 30px;"></i>
+                    `
+                });
+
+                new Vue({
+                    el: '#my_video',
+                    template: '<a class="collect" href="/">범죄자 패턴 분석</a>'
+                });
+
+                new Vue({
+                    el: '#account'
+                });
             }
         },
         error: function(request,status,error) { 
--- management_panel/server/router/main_route.js
+++ management_panel/server/router/main_route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const requestIp = require('request-ip');
 const checkToken = require('../oauth_token/checkTokens');
+const authCheck = require('../oauth_token/authCheck');
 const router = express.Router();
 
-router.use('/', express.static('../public/views'));
 router.use(express.static('../public/css'));
 router.use(express.static('../public/client'));
 router.use(express.static('../public/favicon'));
@@ -13,8 +13,12 @@ router.use(function timeLog(req, res, next) {
     next();
 });
 
-router.get('/:id', checkToken, function(req, res, next) {
-    res.sendFile(path.join(__dirname, '..', '..', '/public/views/test.html'));
+router.get('/', checkToken, function(req, res, next) {
+    res.sendFile(path.join(__dirname, '..', '..', '/public/views/index.html'));
+});
+
+router.get('/access_warnings', authCheck, function(req, res, next) {
+    res.sendFile(path.join(__dirname, '..', '..', '/public/views/warnings.html'));
 });
 
 module.exports = router;
