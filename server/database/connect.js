d863de4408ab9633bb457dc6da9b03e3a83feed5
jun0911 <jun.0911@daum.net>
2021-09-10 12:36:19 +0900

    mongoDB Schema UPDATE

--- server/database/connect.js
+++ server/database/connect.js
@@ -26,4 +26,7 @@ function closedb() {
 // model Schema Save
 module.exports.User = models.User;
 module.exports.Token = models.Token;
+module.exports.Level = models.Level;
+module.exports.Recipe = models.Recipe;
+module.exports.Restaurant = models.Restaurant;
 module.exports.close = closedb;
--- server/database/schema.js
+++ server/database/schema.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { Binary, ObjectID } = require("mongodb");
 
 const UserSchema = new mongoose.Schema({
   user_name: { type: String },
@@ -15,5 +14,35 @@ const TokenSchema = new mongoose.Schema({
   created: { type: Date, default: Date.now },
 });
 
+const veganLevelSchema = new mongoose.Schema({
+  user_id: { type: String },
+  vegan_level: { type: String },
+  vegan_point: { type: Number },
+  diet: { type: String },
+});
+
+const RecipeSchema = new mongoose.Schema({
+  write_name: { type: String },
+  write_title: { type: String },
+  write_summary: { type: String },
+  write_image: { type: String },
+  content: { type: String },
+  writer: { type: String },
+  reputation: { type: Number },
+  created: { type: Date, default: Date.now },
+});
+
+const RestaurantSchema = new mongoose.Schema({
+  restaurant_name: { type: String },
+  restaurant_id: { type: Number },
+  latitude: { type: Number },
+  grade: { type: Number },
+  owner: { type: String },
+  created: { type: Date, default: Date.now },
+});
+
 module.exports.User = mongoose.model("user_db", UserSchema);
 module.exports.Token = mongoose.model("token_db", TokenSchema);
+module.exports.Level = mongoose.model("veganLevel_db", veganLevelSchema);
+module.exports.Recipe = mongoose.model("recipe_db", RecipeSchema);
+module.exports.Restaurant = mongoose.model("restaurant_db", RestaurantSchema);
