59041d5f876e3ce2618253a485fc466e95ede6ee
jun0911-dev <serverwon11112@gmail.com>
2021-09-17 01:34:35 +0900

    router

--- server/router/veganRecipe_route.js
+++ server/router/veganRecipe_route.js
@@ -18,43 +18,9 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 
-let recipeName = {
-  get recipe() {
-    if (this.name) {
-      return this.name;
-    } else {
-      return "Null recipeName";
-    }
-  },
-  set recipe(name) {
-    this.name = name;
-  },
-};
-
 // recipe router
-router.get("/", checkToken, function (req, res, next) {
-  res.sendFile(
-    path.join(__dirname, "..", "..", "/public/views/recipe_list.html")
-  );
-});
-
-router.get("/:recipeName", async function (req, res, next) {
-  recipeName.recipe = await req.params.recipeName;
-  res.sendFile(
-    path.join(__dirname, "..", "..", "/public/views/recipe_page.html")
-  );
-});
-
-router.get("/write/:userToken", function (req, res, next) {
-  res.sendFile(
-    path.join(__dirname, "..", "..", "/public/views/recipe_write.html")
-  );
-});
-
-router.get("/suggestion", dietCheck, function (req, res, next) {
-  res.sendFile(
-    path.join(__dirname, "..", "..", "/public/views/recipe_suggestion.html")
-  );
+router.get("/", dietCheck, function (req, res, next) {
+  res.sendFile(path.join(__dirname, "..", "..", "/public/views/recipe.html"));
 });
 
 router.get("/diet", checkToken, function (req, res, next) {
