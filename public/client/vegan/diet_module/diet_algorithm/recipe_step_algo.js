d1b7f1f9e4595f3d8e841d6968e61c9f371d7b85
jun0911 <jun.0911@daum.net>
2021-09-18 09:15:00 +0900

    Update recipe_step_algo.js

--- public/client/vegan/diet_module/diet_algorithm/recipe_step_algo.js
+++ public/client/vegan/diet_module/diet_algorithm/recipe_step_algo.js
@@ -1,9 +1,25 @@
-let recipe_name_list = [];
-let vegan_step_list = [];
-let recipe_ingredient_list = {};
+class recipe_list {
+  constructor() {
+    this.recipeName_list = ["test"];
+    this.veganStep_list = ["test"];
+    this.recipeIngredient_list = ["test"];
+  }
+  getReicpeList() {
+    let recipe_object = {
+      recipeName: this.recipeName_list,
+      veganStep: this.veganStep_list,
+      recipeIngredient: this.recipeIngredient_list,
+    };
+    return recipe_object;
+  }
+}
+
+const recipeList = new recipe_list();
+
+console.log(recipeList.getReicpeList());
 
 function vegan_step_prototype(recipe_name) {
-  this.recipe_name = recipe_name;
+  return recipe_name;
 }
 
 vegan_step_prototype.prototype.test = (x) => {
@@ -12,5 +28,6 @@ vegan_step_prototype.prototype.test = (x) => {
 
 export const step_settingAlgo = (recipe_name) => {
   document.getElementById("title").innerText = "비건 단계를 설정하는중 ...";
-  getStart(recipe_name);
+  let recipe = new vegan_step_prototype(recipe_name);
+  console.log(recipe);
 };
