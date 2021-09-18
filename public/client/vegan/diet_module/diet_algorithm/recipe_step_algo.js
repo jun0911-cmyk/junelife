8bf2ea9aa75b1be42eb6d0c01391706a80198066
jun0911 <jun.0911@daum.net>
2021-09-18 11:10:31 +0900

    Update recipe_step_algo.js

--- public/client/vegan/diet_module/diet_algorithm/recipe_step_algo.js
+++ public/client/vegan/diet_module/diet_algorithm/recipe_step_algo.js
@@ -1,14 +1,30 @@
 class recipe_list {
-  constructor() {
-    this.recipeName_list = ["test"];
-    this.veganStep_list = ["test"];
-    this.recipeIngredient_list = ["test"];
+  constructor(recipe_name) {
+    this.recipeName_list = recipe_name;
+    this.veganStep_list = [
+      "플렉시테리언",
+      "폴로베지테리언",
+      "페스코베지테리언",
+      "락토오보베지테리언",
+      "오보베지테리언",
+      "락토베지테리언",
+      "비건",
+    ];
+    this.recipeIngredient_object = {
+      flexitarian: ["소고기", "닭고기", "생선", "단백질", "칼슘", "야채"],
+      pollovegetarian: ["닭고기", "생선", "단백질", "칼슘", "야채"],
+      pescovegetarian: ["생선", "단백질", "칼슘", "야채"],
+      lactoovovegetarian: ["단백질", "칼슘", "야채"],
+      ovovegetarian: ["칼슘", "단백질"],
+      lactovegetarian: ["칼슘", "야채"],
+      vegan: ["야채"],
+    };
   }
   getReicpeList() {
     let recipe_object = {
       recipeName: this.recipeName_list,
       veganStep: this.veganStep_list,
-      recipeIngredient: this.recipeIngredient_list,
+      recipeIngredient: this.recipeIngredient_object,
     };
     return recipe_object;
   }
